faa13a3ff1d46ca02425c8337939098f2dcdc05a
xerocross <nonya@business.com>
2019-05-02 08:31:48 -0400

    add error handler basic

--- src/shopping-total.js
+++ src/shopping-total.js
@@ -1,6 +1,14 @@
 import {ShoppingTotal} from "./index.js";
 import Vue from "vue";
 
+
+Vue.config.errorHandler = function (err, vm, info)  {
+    console.log('[Global Error Handler]: Error in ' + info + ': ' + err);
+    console.log(err);
+    console.log(vm);
+    console.log(info);
+};
+
 new Vue({
     el : "#shopping-total",
     components : {
